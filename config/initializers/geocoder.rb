b833734c62f314d683f691e360996f9c5902e554
George Amolsch <gamolsch@gmail.com>
2014-06-07 20:40:55 -0500

    chore(initializer): adds comments and extra functionality to geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,14 @@
 Geocoder.configure(
+
+  #geocoding service
   lookup: :google,
+
+  #IP address geocoding service
+  :ip_lookup => :freegeoip,
+
+  #geocoding service request timeout, in seconds
+  :timeout => 5,
+
+  #caching
   cache: Rails.cache
 )
